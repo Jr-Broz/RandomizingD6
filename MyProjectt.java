ff0bc46cbd7651c37260782ff5a11311444afb46
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-09 09:12:48 -0300

    uploaded MyProjectt.java
    Really Simple RPG D6 Randomizer.

---
+++ MyProjectt.java
@@ -0,0 +1,25 @@
+package com.mycompany.myprojectt;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyProjectt {
+
+    public static void main(String[] args) {
+        
+    Random randomizar = new Random();
+        
+        System.out.println("Write how many times u want to roll dice");
+        
+        Scanner resposta  = new Scanner(System.in);
+        
+        System.out.println("=================");
+       int resposta2 = resposta.nextInt();
+        System.out.println("=================");
+        for(int i = 0; i < resposta2;i++){
+        
+           
+            int d7 = randomizar.nextInt(7);
+            System.out.println(d7);       
+        } 
+    }      
+}
